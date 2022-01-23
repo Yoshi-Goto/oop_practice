b64d9a8e6cce238c259656f39a0f262df142c063
Yoshi-Goto <gotech@yahoo.co.jp>
2022-01-23 18:09:10 +0900

    再集計かな！！

--- bmi.py
+++ bmi.py
@@ -17,7 +17,12 @@ class *:
 class BMI:
     def __init__(self, height_m, weight_kg):
         self.value = weight_kg / pow(height_m, 2)
+        if not (10 <= self.value <= 40):
+            raise ValueError(f'BMI血が規定値を外れています。！！！{self.value}')
+
+    def __str__(self):
+        return f"{self.value:.2f}"
 
 
 yg = BMI(height_m=1.7, weight_kg=65)
-print(yg.value)
+print(yg)
